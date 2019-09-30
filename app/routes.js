fc5d3c30e5764cea1df7e59abdfd51bd9e485a5e
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-09-30 01:01:21 -0500

    first commit

---
+++ app/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PersonController = require('./controllers/PersonController');
+
+const router = express.Router();
+
+// Persons Routes
+router.get('/persons/:id(\\d+)', PersonController.find);
+router.post('/persons', PersonController.save);
+router.delete('/persons/:id(\\d+)', PersonController.delete);
+router.put('/persons/:id(\\d+)', PersonController.edit);
+router.get('/persons/find_by_name', PersonController.findByName);
+router.get('/persons/all', PersonController.listAll);
+
+module.exports = router;
