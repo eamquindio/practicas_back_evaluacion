f3899736a36632eee84965f5dafd8d457376578b
Nicolas Davila <nikojako99@gmail.com>
2019-10-02 16:15:13 -0500

    [resolves #9] circleci correction

--- app/controllers/EvaluationController.js
+++ app/controllers/EvaluationController.js
@@ -20,6 +20,7 @@ EvaluationController.find = async (req, res, next) => {
     const { params: { id } } = req;
     const evaluation = await EvaluationService.find(id);
     if (!evaluation) return next(new ErrorHandler.BaseError('evaluation not exists', 404));
+    
     return res.send(evaluation);
   } catch (error) {
     console.log(error);
