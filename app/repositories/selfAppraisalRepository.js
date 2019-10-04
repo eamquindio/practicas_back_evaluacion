884ac567dc106e56da5ffd0af1cd8dcd01b150da
Nicolas Davila <nikojako99@gmail.com>
2019-10-04 14:14:54 -0500

    [resolves #4] Correction of errors circleci

--- app/repositories/selfAppraisalRepository.js
+++ app/repositories/selfAppraisalRepository.js
@@ -2,4 +2,4 @@ const SelfAppraisalRepository = module.exports;
 const DB = require('../utils/DB');
 
 SelfAppraisalRepository.listAll = () => DB('selfAppraisal').select('*');
-SelfAppraisalRepository.create =selfAppraisal => DB('selfAppraisal').insert(selfAppraisal).returning('*');
+SelfAppraisalRepository.create = selfAppraisal => DB('selfAppraisal').insert(selfAppraisal).returning('*');
