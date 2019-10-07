46f96c728dd075333d53ceafae164a6dc77cedaa
Gabriela <gabrielaaa1413@gmail.com>
2019-10-07 15:19:34 -0500

    [resolve #6] find evaluation

--- app/repositories/EvaluacionRepository.js
+++
@@ -1,6 +0,0 @@
-const EvaluacionRepository = module.exports;
-const DB = require('../utils/DB');
-
-EvaluacionRepository.create = evaluacion => DB('evaluaciones').insert(evaluacion).returning('*');
-
-EvaluacionRepository.find = id => DB('evaluaciones').select('*').where({ id }).first();
