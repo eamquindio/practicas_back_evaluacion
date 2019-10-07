11c5d67f1b56b12c7e81a5e7df97d85f70d829ad
conrado94 <48019268+conrado94@users.noreply.github.com>
2019-10-07 15:49:21 -0500

    ya

--- app/controllers/selfAppraisalController.js
+++ app/controllers/selfAppraisalController.js
@@ -2,7 +2,7 @@ const SelfAppraisalController = module.exports;
 const SelfAppraisalService = require('../services/selfAppraisalService');
 
 
-SelfAppraisalController.listAll = async (req, res, next) => {
+SelfAppraisalController.listAll = async (res) => {
   try {
     const selfAppraisal = await SelfAppraisalService.listAll();
     if (selfAppraisal.length === 0) return res.status(204).send(selfAppraisal);
