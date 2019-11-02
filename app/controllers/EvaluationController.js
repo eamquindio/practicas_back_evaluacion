0ac6e6c34cd55fcb0c3a6e02448a4e51cc4e4534
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-11-02 10:37:16 -0500

    fix logs

--- app/controllers/EvaluationController.js
+++ app/controllers/EvaluationController.js
@@ -4,7 +4,7 @@ const ErrorHandler = require('../utils/ErrorHandlerMiddleware');
 
 EvaluationController.save = async (req, res, next) => {
   const { body } = req;
-  console.log(`EvaluationController.save ${body}`);
+  console.log(`EvaluationController.save ${JSON.stringify(body)}`);
   try {
     await EvaluationService.create(body);
 
